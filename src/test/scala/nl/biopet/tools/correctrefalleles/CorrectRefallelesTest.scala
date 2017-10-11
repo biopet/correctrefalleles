1eb47fa3d4ccd8ed83647ae55737d8f931296f6b
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/correctrefalleles/CorrectRefallelesTest.scala
+++ src/test/scala/nl/biopet/tools/correctrefalleles/CorrectRefallelesTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.correctrefalleles
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object CorrectRefallelesTest extends BiopetTest {
+class CorrectRefallelesTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
