d285b38c8ad24ab3c4101d3a0b8ae84e73d26b16
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-01 12:53:04 +0100

    enabled test

--- src/test/scala/nl/biopet/tools/correctrefalleles/CorrectRefAllelesTest.scala
+++ src/test/scala/nl/biopet/tools/correctrefalleles/CorrectRefAllelesTest.scala
@@ -4,6 +4,7 @@ import nl.biopet.utils.test.tools.ToolTest
 import org.testng.annotations.Test
 
 class CorrectRefAllelesTest extends ToolTest[Args] {
+  def toolCommand: CorrectRefAlleles.type = CorrectRefAlleles
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
