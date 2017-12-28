8a855cb19713a2cb97bb6e184ee914a641bd3ab6
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-12-28 11:02:29 +0100

    remove unbounded

--- src/main/scala/nl/biopet/tools/correctrefalleles/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/correctrefalleles/ArgsParser.scala
@@ -6,15 +6,15 @@ import nl.biopet.utils.tool.{AbstractOptParser, ToolCommand}
 
 class ArgsParser(toolCommand: ToolCommand[Args])
     extends AbstractOptParser[Args](toolCommand) {
-  opt[File]('I', "input") required () unbounded () valueName "<vcf file>" action {
+  opt[File]('I', "input") required () valueName "<vcf file>" action {
     (x, c) =>
       c.copy(inputFile = x)
   } text "input vcf file"
-  opt[File]('o', "output") required () unbounded () valueName "<vcf file>" action {
+  opt[File]('o', "output") required () valueName "<vcf file>" action {
     (x, c) =>
       c.copy(outputFile = x)
   } text "output vcf file"
-  opt[File]('R', "referenceFasta") required () unbounded () valueName "<fasta file>" action {
+  opt[File]('R', "referenceFasta") required () valueName "<fasta file>" action {
     (x, c) =>
       c.copy(referenceFasta = x)
   } text "Reference fasta file"
